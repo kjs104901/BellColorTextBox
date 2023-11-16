0c649aa19c3b38c4e50438238e94fe802d5b58cf
Kim Jinsu <kjs104901@gmail.com>
2023-11-17 06:47:51 +0900

    Dev off

--- BellColorTextBox.Net/BellColorTextBox.Net/Utils/DevHelper.cs
+++ BellColorTextBox.Net/BellColorTextBox.Net/Utils/DevHelper.cs
@@ -3,7 +3,7 @@
 public static class DevHelper
 {
 #if DEBUG
-    public const bool IsDebugMode = true;
+    public const bool IsDebugMode = false;
 #else
     public const bool IsDebugMode = false;
 #endif
